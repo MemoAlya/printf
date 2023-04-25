fe93096847b5a49d61daa82ab906c5d46d76a0c6
MemoAlya <memorygwanunga@gmail.com>
2023-04-25 09:31:06 -0700

    get precision

--- get_precision.c
+++ get_precision.c
@@ -2,40 +2,36 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @format: Fprmatted string in which to print the arguments
+ * @list: list of arguments
  *
- * Return: Precision.
+ * Return: Precision, or -1 if no precision was specified.
  */
-int get_precision(const char *format, int *i, va_list list)
+
+int get_precision(const char *format, va_list list)
 {
-	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
+	const char *precision_start = strchr(format, '.');
 
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	if (precision_start != NULL)
 	{
-		if (is_digit(format[curr_i]))
+		const char *precisio_end = precision_start + 1;
+
+		if (*precision_end == '*')
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
+			precision == va_arg(list, int);
 		}
 		else
-			break;
+		{
+			precision = 0;
+			while (isdigit(*precison_end))
+			{
+				precision = precisin * 10 + (*precision_end - '0');
+				precision_end++;
+			}
+		}
 	}
-
-	*i = curr_i - 1;
 
 	return (precision);
 }
